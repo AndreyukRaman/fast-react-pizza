a224682ff956600f847ff6b33213af439defc1e7
Raman Andreyuk <romaandreyuk@gmail.com>
2024-03-24 00:24:41 +0100

    Using Flexbox

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -2,9 +2,9 @@ import CreateUser from "../features/user/CreateUser"
 
 function Home() {
   return (
-    <div className='my-10 sm:my-16 text-center'>
+    <div className='my-10 sm:my-16 text-center px-4'>
       <h1
-        className="text-xl font-semibold text-center mb-8
+        className="text-xl font-semibold text-center mb-8 md:text-3xl
     "
       >
         The best pizza.
