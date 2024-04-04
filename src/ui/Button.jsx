40f8b90e759adccd7f900a6f485d48b64a669395
Raman Andreyuk <romaandreyuk@gmail.com>
2024-04-04 21:20:24 +0200

    Adding Menu Items to The Cart

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 
-function Button({children, disabled,to, type}) {
+function Button({children, disabled,to, type, onClick}) {
  
 const base =
   'bg-yellow-400 uppercase font-semibold text-sm text-stone-800  inline-block tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed disabled:bg-slate-600 ';
@@ -13,6 +13,14 @@ secondary: ' uppercase font-semibold text-sm text-stone-400  inline-block tracki
 
   if (to) 
   return <Link to={to} className={styles[type]}>{children}</Link>
+
+if(onClick) 
+ return (
+    <button onClick={onClick} disabled={disabled} className={styles[type]}>
+      {children}
+    </button>
+  )
+
   return (
     <button disabled={disabled} className={styles[type]}>
       {children}
