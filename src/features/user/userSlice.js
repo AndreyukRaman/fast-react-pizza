7c9c45e6f29d7fdb4cfa098549cedefc2f794a06
Raman Andreyuk <romaandreyuk@gmail.com>
2024-04-08 15:36:52 +0200

    Integrating Geolocation

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -51,7 +51,7 @@ builder.addCase(fetchAddress.pending, (state,action) => {state.status = 'loading
 .addCase(fetchAddress.fulfilled, (state,action)=>  {state.position = action.payload.position; 
   state.address = action.payload.address; 
   state.status = 'idle'})
-  .addCase(fetchAddress.rejected, (state,action) => {state.status = 'error'; state.error = action.error.message;
+  .addCase(fetchAddress.rejected, (state,action) => {state.status = 'error'; state.error = 'There was a problem getting your address. Make sure to fill this field';
 }),
 
 })
